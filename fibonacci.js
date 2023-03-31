67f2ed890aed35ff772a9b96318a499da5ccb0a7
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-03-31 09:50:39 -0300

    add recursive solution

--- fibonacci.js
+++ fibonacci.js
@@ -10,7 +10,27 @@ function fibs(n) {
       fib[i] = fib[i - 1] + fib[i - 2];
     }
   }
-  console.log(fib);
+  return fib;
 }
 
-fibs(8);
+// console.log(fibs(8));
+
+// recursive approach
+
+function fibsRec(n) {
+  let fib = [];
+
+  if (n <= 0) {
+    return fib;
+  } else if (n === 1) {
+    fib.push(0);
+  } else if (n === 2) {
+    fib.push(0, 1);
+  } else {
+    fib = fibsRec(n - 1);
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return fib;
+}
+
+console.log(fibsRec(8));
